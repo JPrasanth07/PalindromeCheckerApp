e22065fae85bec9ebde6fac2d724368317ebd456
JPrasanth07 <jampulaprashanth7591@gmail.com>
2026-02-27 16:14:48 +0530

    UC2

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,7 +1,17 @@
- public class PalindromeCheckerApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome To Palindrome Checker Management System");
-        System.out.println("Version:1.0");
-        System.out.println("system initialized successfully");
+        String original = "madam";
+        String reversed = "";
+
+        for(int i = original.length() - 1; i >= 0; --i) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("The string '" + original + "' is a palindrome.");
+        } else {
+            System.out.println("The string '" + original + "' is NOT a palindrome.");
+        }
+
     }
 }
