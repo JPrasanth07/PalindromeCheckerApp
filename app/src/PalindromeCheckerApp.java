1f5a76241cd506c6b3e3ef1353af069674c4f226
JPrasanth07 <jampulaprashanth7591@gmail.com>
2026-03-16 14:36:41 +0530

    uc5

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,7 +1,28 @@
- public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Palindrome Checker Management System");
-        System.out.println("Version:1.0");
-        System.out.println("system initialized successfully");
+import java.util.Stack;
+
+    public class PalindromeCheckerApp {
+        public static void main(String[] args) {
+
+            String input = "noon";
+
+            Stack<Character> stack = new Stack<>();
+
+
+            for (char c : input.toCharArray()) {
+                stack.push(c);
+            }
+
+            boolean isPalindrome = true;
+
+
+            for (char c : input.toCharArray()) {
+                if (c != stack.pop()) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            System.out.println("Input: " + input);
+            System.out.println("Is Palindrome? " + isPalindrome);
+        }
     }
-}
